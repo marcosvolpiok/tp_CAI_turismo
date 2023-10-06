cc54924f6326f5d076212ef5345f47846fcd2f85
Marcos Volpi <mark.landia@gmail.com>
2023-10-06 19:16:03 -0300

    Añade botón "Buscar" a formulario "Consultar Reserva"

--- TPCAI/FormConsultarReservas.Designer.cs
+++ TPCAI/FormConsultarReservas.Designer.cs
@@ -36,6 +36,7 @@
             this.ColNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColumnEstado = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -97,11 +98,21 @@
             this.button1.Text = "Volver al Menú";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(379, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 26);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Buscar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // FormConsultarReservas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(670, 522);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.textBox1);
@@ -124,5 +135,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn ColNombre;
         private System.Windows.Forms.DataGridViewTextBoxColumn ColumnEstado;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
