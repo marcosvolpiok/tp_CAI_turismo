8a1a110442e897c3a1f8d06f6bfca45e3630aa6d
Marcos Volpi <mark.landia@gmail.com>
2023-10-10 19:46:54 -0300

    Remueve botón de menú principal

--- TPCAI/FromMenu.Designer.cs
+++ TPCAI/FromMenu.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
@@ -37,25 +36,16 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(267, 100);
+            this.button1.Location = new System.Drawing.Point(147, 43);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(143, 42);
             this.button1.TabIndex = 0;
             this.button1.Text = "Consultar Productos";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(267, 184);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(143, 42);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Armar Presupuesto";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(267, 273);
+            this.button3.Location = new System.Drawing.Point(147, 214);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(143, 42);
             this.button3.TabIndex = 2;
@@ -64,7 +54,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(267, 364);
+            this.button4.Location = new System.Drawing.Point(147, 125);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(143, 42);
             this.button4.TabIndex = 3;
@@ -73,7 +63,7 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(267, 442);
+            this.button5.Location = new System.Drawing.Point(147, 301);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(143, 42);
             this.button5.TabIndex = 4;
@@ -84,11 +74,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(709, 550);
+            this.ClientSize = new System.Drawing.Size(510, 467);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "FromMenu";
             this.Text = "Menú";
@@ -99,7 +88,6 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
