2836b2f37020919e52cd272d2e0c3f73ea478b81
Marcos Volpi <mark.landia@gmail.com>
2023-10-13 10:33:43 -0300

    Borra formularios para dejar solo lo de vuelos y alojamientos

--- TPCAI/FormMenuProductos.Designer.cs
+++ TPCAI/FormMenuProductos.Designer.cs
@@ -28,21 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnConsultarCruceros = new System.Windows.Forms.Button();
             this.btnConsultarAlojamiento = new System.Windows.Forms.Button();
-            this.btnConsultarPaquetes = new System.Windows.Forms.Button();
             this.btnConsultarVuelos = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // btnConsultarCruceros
-            // 
-            this.btnConsultarCruceros.Location = new System.Drawing.Point(85, 281);
-            this.btnConsultarCruceros.Name = "btnConsultarCruceros";
-            this.btnConsultarCruceros.Size = new System.Drawing.Size(114, 40);
-            this.btnConsultarCruceros.TabIndex = 11;
-            this.btnConsultarCruceros.Text = "Consular Cruceros";
-            this.btnConsultarCruceros.UseVisualStyleBackColor = true;
-            this.btnConsultarCruceros.Click += new System.EventHandler(this.btnConsultarCruceros_Click);
             // 
             // btnConsultarAlojamiento
             // 
@@ -53,16 +41,6 @@
             this.btnConsultarAlojamiento.Text = "Consular Alojamientos";
             this.btnConsultarAlojamiento.UseVisualStyleBackColor = true;
             this.btnConsultarAlojamiento.Click += new System.EventHandler(this.btnConsultarAlojamiento_Click);
-            // 
-            // btnConsultarPaquetes
-            // 
-            this.btnConsultarPaquetes.Location = new System.Drawing.Point(85, 211);
-            this.btnConsultarPaquetes.Name = "btnConsultarPaquetes";
-            this.btnConsultarPaquetes.Size = new System.Drawing.Size(114, 40);
-            this.btnConsultarPaquetes.TabIndex = 9;
-            this.btnConsultarPaquetes.Text = "Consular Paquetes";
-            this.btnConsultarPaquetes.UseVisualStyleBackColor = true;
-            this.btnConsultarPaquetes.Click += new System.EventHandler(this.btnConsultarPaquetes_Click);
             // 
             // btnConsultarVuelos
             // 
@@ -79,21 +57,17 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 393);
-            this.Controls.Add(this.btnConsultarCruceros);
             this.Controls.Add(this.btnConsultarAlojamiento);
-            this.Controls.Add(this.btnConsultarPaquetes);
             this.Controls.Add(this.btnConsultarVuelos);
             this.Name = "FormMenuProductos";
-            this.Text = "Menu Productos";
+            this.Text = "FormMenuProductos";
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnConsultarCruceros;
         private System.Windows.Forms.Button btnConsultarAlojamiento;
-        private System.Windows.Forms.Button btnConsultarPaquetes;
         private System.Windows.Forms.Button btnConsultarVuelos;
     }
 }
