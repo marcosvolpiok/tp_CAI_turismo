da76948df58e44f8fd323447ec1bb5b7ed6dc3a4
Marcos Volpi <mark.landia@gmail.com>
2023-10-14 12:34:09 -0300

    Ticket Whatsapp de Nico Pare: 3. en las pantallas de listado (posterior a consulta de productos), agregar algún minimo mensaje al estilo "Seleccione el producto que desee agregar al presupuesto"

--- TPCAI/FormListadoAlojamiento.Designer.cs
+++ TPCAI/FormListadoAlojamiento.Designer.cs
@@ -39,6 +39,7 @@
             this.btnVerPresupuesto = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewListadoAlojamiento)).BeginInit();
             this.SuspendLayout();
             // 
@@ -115,7 +116,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 13);
+            this.label1.Location = new System.Drawing.Point(186, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(139, 13);
             this.label1.TabIndex = 7;
@@ -124,17 +125,28 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(27, 36);
+            this.label2.Location = new System.Drawing.Point(24, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(137, 13);
             this.label2.TabIndex = 8;
             this.label2.Text = "Fecha Egreso: 15/03/2024";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(472, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Seleccione el producto que desee agregar al presupuesto";
             // 
             // FormListadoAlojamiento
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1013, 625);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnVerPresupuesto);
@@ -163,5 +175,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn ColumnTipoHabitacion;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
