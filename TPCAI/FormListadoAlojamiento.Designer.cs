ff5821845a8297f8babf107ca429537596f42570
Marcos Volpi <mark.landia@gmail.com>
2023-11-01 21:27:24 -0300

    Ticket 41. Listado aloj -> Borrar “añadir a nro presupuesto”

--- TPCAI/FormListadoAlojamiento.Designer.cs
+++ TPCAI/FormListadoAlojamiento.Designer.cs
@@ -34,13 +34,10 @@
             this.ColumnTarifa = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColumnCalificacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColumnTipoHabitacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.button1 = new System.Windows.Forms.Button();
             this.btnVerPresupuesto = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.BtnBuscar = new System.Windows.Forms.Button();
             this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
@@ -103,15 +100,6 @@
             this.ColumnTipoHabitacion.HeaderText = "Tipo Habitacion";
             this.ColumnTipoHabitacion.Name = "ColumnTipoHabitacion";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(588, 576);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(122, 31);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Añadir a Presupuesto";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // btnVerPresupuesto
             // 
             this.btnVerPresupuesto.Location = new System.Drawing.Point(1006, 576);
@@ -149,22 +137,6 @@
             this.label3.Size = new System.Drawing.Size(472, 20);
             this.label3.TabIndex = 9;
             this.label3.Text = "Seleccione el producto que desee agregar al presupuesto";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(458, 587);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(124, 20);
-            this.textBox1.TabIndex = 10;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(458, 571);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(81, 13);
-            this.label4.TabIndex = 11;
-            this.label4.Text = "N° Presupuesto";
             // 
             // comboBox2
             // 
@@ -353,14 +325,11 @@
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label11);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnVerPresupuesto);
             this.Controls.Add(this.dataGridViewListadoAlojamiento);
-            this.Controls.Add(this.button1);
             this.Name = "FormListadoAlojamiento";
             this.Text = "Listado Alojamiento";
             this.Load += new System.EventHandler(this.FormListadoAlojamiento_Load);
@@ -373,7 +342,6 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridViewListadoAlojamiento;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btnVerPresupuesto;
         private System.Windows.Forms.DataGridViewTextBoxColumn ColumnDestino;
         private System.Windows.Forms.DataGridViewTextBoxColumn ColumnNombreAlojamiento;
@@ -383,8 +351,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Button BtnBuscar;
         private System.Windows.Forms.DateTimePicker dateTimePicker2;
