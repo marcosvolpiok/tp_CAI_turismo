bce74ca822b63b912a036a8a082c018e13080f5d
Marcos Volpi <mark.landia@gmail.com>
2023-11-01 21:43:48 -0300

    Ticket 56. Add pasajero -> Add btn volver a presupuesto

--- TPCAI/FormAñadirPasajero.Designer.cs
+++ TPCAI/FormAñadirPasajero.Designer.cs
@@ -44,11 +44,12 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label10 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnGuardar
             // 
-            this.btnGuardar.Location = new System.Drawing.Point(187, 354);
+            this.btnGuardar.Location = new System.Drawing.Point(84, 354);
             this.btnGuardar.Name = "btnGuardar";
             this.btnGuardar.Size = new System.Drawing.Size(122, 31);
             this.btnGuardar.TabIndex = 17;
@@ -186,11 +187,21 @@
             this.label10.TabIndex = 58;
             this.label10.Text = "Presupuesto Activo: 4545";
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(293, 354);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 31);
+            this.button3.TabIndex = 59;
+            this.button3.Text = "Volver a Presupuesto";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // FormAñadirPasajero
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(492, 413);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -232,5 +243,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button3;
     }
 }
