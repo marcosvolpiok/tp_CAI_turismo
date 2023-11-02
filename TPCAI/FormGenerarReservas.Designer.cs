804da26b9c460abc6ba2dc141ea7689341d4897c
Marcos Volpi <mark.landia@gmail.com>
2023-11-01 21:42:32 -0300

    Ticket 50. Generar presupuesto -> Add btn Iniciar nuevo presupuesto Ticket 51. Generar presupuesto -> Btn “Establecer como activo”

--- TPCAI/FormGenerarReservas.Designer.cs
+++ TPCAI/FormGenerarReservas.Designer.cs
@@ -53,6 +53,8 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.btnVolverMenu = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabGenerarPreReserva.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPreReserva)).BeginInit();
@@ -101,6 +103,8 @@
             // 
             // tabGenerarPreReserva
             // 
+            this.tabGenerarPreReserva.Controls.Add(this.button2);
+            this.tabGenerarPreReserva.Controls.Add(this.button1);
             this.tabGenerarPreReserva.Controls.Add(this.btnAñadirPasajeros);
             this.tabGenerarPreReserva.Controls.Add(this.dataGridViewPreReserva);
             this.tabGenerarPreReserva.Controls.Add(this.btnPreReservar);
@@ -273,6 +277,24 @@
             this.btnVolverMenu.UseVisualStyleBackColor = true;
             this.btnVolverMenu.Click += new System.EventHandler(this.btnVolverMenu_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(567, 507);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 31);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Establecer como Activo";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(412, 507);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 31);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Iniciar Nuevo Presupuesto";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // FormGenerarReservas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -325,5 +347,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn8;
         private System.Windows.Forms.Button btnVolverMenu;
         private System.Windows.Forms.Button btnAñadirPasajeros;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
