f15eb0cbbb5db90816205116554bc2a4d7d5b0d0
Marcos Volpi <mark.landia@gmail.com>
2023-11-04 14:30:31 -0300

    Ticket #860rytvbc - Modelo Form FormListadoAlojamiento Añade clases y sub clases

---
+++ TPCAI/Entidades/AlojamientosEnt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TPCAI
+{
+    class AlojamientosEnt
+    {
+        public long IDAlojamientos { get; set; }
+        public string CodigoHotel { get; set; }
+        public string Nombre { get; set; }
+        public string CodigoCiudad { get; set; }
+        public string Direccion { get; set; }
+        public string Calificacion { get; set; }
+        public List<DisponibilidadSubClass> Disponibilidad { get; set; }
+        
+
+
+    }
+}
---
+++ TPCAI/Entidades/SubClasses/DisponibilidadSubClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPCAI
+{
+    class DisponibilidadSubClass
+    {
+        public string Nombre { get; set; }
+        public decimal Tarifa { get; set; }
+        public int Capacidad { get; set; }
+        public int Menores { get; set; }
+        public int Infantes { get; set; }
+        public List<HabitacionesHotelSubClass> Habitaciones { get; set; }
+        public long IDDisponibilidad { get; set; }
+    }
+}
