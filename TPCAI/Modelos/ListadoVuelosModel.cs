a5a8ac746245a49e58ab151e7d484ca5eed27150
NicoPare <parenicolas98@gmail.com>
2023-11-06 01:58:13 -0300

    Cambios en formulario Vuelos

---
+++ TPCAI/Modelos/ListadoVuelosModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPCAI.Modulos;
+using TPCAI.Entidades.SubClasses;
+using TPCAI.Almacenes;
+using TPCAI.Modelos;
+
+namespace TPCAI.Modelos
+{
+    public class ListadoVuelosModel
+    {
+
+        public string Origen { get; set; }
+        public string Destino { get; set; }
+        public DateTime FechaIda { get; set; }
+        public DateTime FechaVuelta { get; set; }
+        public int CantAdultos { get; set; }
+        public int CantMenores { get; set; }
+        public int CantInfantes { get; set; }
+        public string Clase { get; set; }
+
+        public List<Vuelo> VuelosFiltrados { get; private set; }
+
+        public void BuscarVuelos()
+        {
+            // Realiza la búsqueda de vuelos y guarda los resultados en VuelosFiltrados.
+            VuelosFiltrados = ProductosModulo.BusquedaVuelos(this);
+        }
+    }
+}
